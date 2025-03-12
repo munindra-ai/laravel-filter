8c1f848d3d00b27d1001f91a27f9f4e4b7b7ec12
munindra-ai <manindratamang5@gmail.com>
2025-03-12 14:34:47 +0545

    :recycle: refac.. comment...

--- src/LaravelFilterServiceProvider.php
+++ src/LaravelFilterServiceProvider.php
@@ -10,11 +10,6 @@ class LaravelFilterServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('laravel-filter')
             ->hasConfigFile()
